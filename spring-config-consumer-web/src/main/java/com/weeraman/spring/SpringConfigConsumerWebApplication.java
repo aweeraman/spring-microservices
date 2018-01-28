870612a866d71004a1550d4c6e41984bbc268eef
Anuradha Weeraman <anuradha@weeraman.com>
2018-01-28 19:03:51 +0530

    Updated

---
+++ spring-config-consumer-web/src/main/java/com/weeraman/spring/SpringConfigConsumerWebApplication.java
@@ -0,0 +1,12 @@
+package com.weeraman.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigConsumerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigConsumerWebApplication.class, args);
+	}
+}
